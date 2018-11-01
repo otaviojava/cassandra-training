fb2344da47262032a54287ff339f7a620b070581
Otavio Santana <otaviopolianasantana@gmail.com>
2018-11-01 17:06:16 -0300

    adiciona regra

---
+++ mapper-datastax/src/main/java/com/nosqlxp/cassandra/App.java
@@ -0,0 +1,14 @@
+package com.nosqlxp.cassandra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Runnable r = () -> System.out.println("");
+    }
+}
